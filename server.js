98a2dbb4b7eac4bbb4c7ed891ea0105b109f43f9
freshMouse <freshMouse223@gmail.com>
2017-06-26 05:15:42 -0500

    server update

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
 
+app.set("port", (process.env.PORT || 5000));
+
 app.use("/src", express.static("src"));
 app.use("/assets", express.static("src/assets"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/src/index.html");
 });
-
-app.listen(80);
-console.log("serving at 80");
+//app.listen(8080);
+app.listen(app.get("port"), function() {
+ console.log("serving at port", app.get("port"));
+});
